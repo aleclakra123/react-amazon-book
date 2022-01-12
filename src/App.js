a04b6c4558c9554a58ac1e9447a17f820ef29a98
Alec Aldrine Lakra <alec.lakra@wingify.com>
2022-01-12 23:31:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import books from "./books.json";
 
-function App() {
+
+const bookCard = books.map(({id, imgSrc, name, author, price, format, starRating, reviewers}) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Book 
+        imgSrc={imgSrc} 
+        name={name} 
+        author={author} 
+        key={id} 
+        price={price} 
+        format={format}
+        reviewers={reviewers}
+        starRating={starRating} />
+  )
+})
+
+function BookList() {
+  return (
+    <section className="bookList">
+     {bookCard}
+    </section>
+  )
 }
 
-export default App;
+function StarRating({star}) {
+  const starsArray = [1, 2, 3, 4, 5];
+  const stars = starsArray.map((item, index)=>{
+    if(index <= star - 1) {
+      return <span className="fa fa-star checked" key={index} />
+    } else {
+      return <span className="fa fa-star" key={index} />
+    }
+  })
+  return (
+    <span>
+      {stars}
+    </span>
+  )
+}
+
+function Book({imgSrc, name, author, price, format, starRating, reviewers}) {
+  return (
+    <div className="card">
+      <img src={imgSrc} alt="Avatar" style={{width: "100%"}}/>
+      <div className="container">
+        <h4 className='bookName'>{name}</h4> 
+        <p className='author'>{author}</p>
+        <div>
+           <StarRating star={starRating} />
+           <span className='reviewers'>{reviewers}</span>
+        </div>
+        <p className='price'>&#8377;{price}</p>
+        <p className='format'>{format}</p> 
+      </div>
+    </div>
+  )
+}
+
+export default BookList;
